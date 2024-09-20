d308892184dc85cc37fd4cab7579929bbfb24374
Kang <129756276+LeiZiKang@users.noreply.github.com>
2024-09-21 01:07:43 +0800

    添加demo

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by 雷子康 on 2024/9/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo/DemoTests/DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTests.swift
+//  DemoTests
+//
+//  Created by 雷子康 on 2024/9/21.
+//
+
+import Testing
+@testable import Demo
+
+struct DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
