442ef4295c93a4e99dff0670b28e0ee013cc2aa2
Kang <129756276+LeiZiKang@users.noreply.github.com>
2024-09-21 02:43:51 +0800

    添加播放器

--- Demo/Demo/VideoPlayerView.swift
+++ Demo/Demo/VideoPlayerView.swift
@@ -12,6 +12,9 @@ struct VideoPlayerView: View {
     var body: some View {
         VStack {
             ZKSwiftUIVideoPlayer(url: "【MV】(搬运)《梨泰院Class》OST合集 朴叙俊 金多美 - 001 - Part.1 Still Fighting It".zk_bundle_mp4()!)
+                .frame(maxWidth: .infinity, alignment: .top)
+
+            Spacer()
         }
     }
 }
